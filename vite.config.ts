52ca883c627781142c5e0b8eeaeca3d0d5e931f3
Hadji <hadji.tiara@gmail.com>
2025-11-18 21:44:17 -0600

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  return {
-    plugins: [react()],
-    base: command === 'build' ? '/eteorico-quiz/' : '/'
-  }
+export default defineConfig({
+  plugins: [react()],
+  // Establece la base URL al nombre de tu repositorio
+  base: '/eteorico-quiz/' 
 })
